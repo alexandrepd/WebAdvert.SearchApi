47ec2107ee9438a7995f894f7e7b80483ee02997
Alexandre Oliveira <alexandre_pd@hotmail.com>
2023-01-14 21:02:12 +0000

    removing warnings

--- WebAdvert.SearchApi/Extensions/AddNestConfigurationExtension.cs
+++ WebAdvert.SearchApi/Extensions/AddNestConfigurationExtension.cs
@@ -7,13 +7,13 @@ namespace WebAdvert.SearchApi.Extensions
     {
         public static void AddElasticSearch(this IServiceCollection services, IConfiguration configuration)
         {
-            string elasticSearchUrl = configuration.GetSection("ES").GetValue<string>("URL");
+            string? elasticSearchUrl = configuration.GetSection("ES").GetValue<string>("URL");
 
-            var connectionString = new ConnectionSettings(new Uri(elasticSearchUrl))
+            ConnectionSettings connectionString = new ConnectionSettings(new Uri(uriString: elasticSearchUrl!))
                 .BasicAuthentication("elastic", "6Mz=ZgQ5j+evfwxJtCzH")
                 .DefaultIndex("adverts")
                 .DefaultMappingFor<AdvertType>(advert => advert.IdProperty(id => id.Id));
-            var client = new ElasticClient(connectionString);
+            ElasticClient client = new ElasticClient(connectionString);
 
             services.AddSingleton<IElasticClient>(client);
         }
--- WebAdvert.SearchApi/Models/AdvertType.cs
+++ WebAdvert.SearchApi/Models/AdvertType.cs
@@ -2,8 +2,8 @@
 {
     public class AdvertType
     {
-        public string Id { get; set; }
-        public string Title { get; set; }
+        public string? Id { get; set; }
+        public string? Title { get; set; }
         public DateTime CreationDateTime { get; set; }
     }
 }
